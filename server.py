bd02fe4c2afa6527b9da7f3dd2dccbe00c6820f3
Sourcery AI <>
2022-06-28 06:58:40 +0000

    'Refactored by Sourcery'

--- server.py
+++ server.py
@@ -24,47 +24,41 @@ def insert_mongo(query):
 
 @app.route('/api1/danci', methods=['GET', ])
 def index():
-    if request.method == 'GET':
-        args = request.args['ci'].strip()
-        if args:
-            result = [col.find_one({'args': args})]
-
-            if result[0] is not None:
-                for res in result:
-                    res["_id"] = str(res["_id"])
-
-                return jsonify({"code": 0, "data": result, "page_total": 1})
-            else:
-                url = "http://dict.youdao.com/w/" + args + "/"
-                soup = BeautifulSoup(requests.get(url).text, features="html.parser")
-                trans = soup.find('div', {'id': 'phrsListTab'}).find_all("ul", limit=1)[0]
-                if trans:
-                    trans = trans.text.strip('\n').split('\n')
-
-                pronounce = soup.find('div', {'id': 'phrsListTab'}).find("div", {"class": "baav"})
-                if pronounce:
-                    pronounce = pronounce.text.strip('\n')
-
-                additional = soup.find('div', {'id': 'phrsListTab'}).find("p", {"class": "additional"})
-                if additional:
-                    additional = additional.text
-                query = {"args": args, "trans": trans, "pronounce": pronounce, "additional": additional}
-                insert_mongo(query)
-
-                result = [col.find_one({'args': args})]
-                for res in result:
-                   res["_id"] = str(res["_id"])
-                return jsonify({"code": 0, "data": result, "page_total": 1})
-        else:
-            page = int(request.args.get('page') or 1)
-            start = (page - 1) * PER_PAGE
-            result = list(col.find().sort('_id', 1).skip(start).limit(PER_PAGE))
-            for res in result:
-                res["_id"] = str(res["_id"])
-
-            return jsonify({"code": 0, "data": result, "page_total": math.ceil(col.count()/13)})
-    else:
+    if request.method != 'GET':
         return jsonify({"code": 200})
+    if args := request.args['ci'].strip():
+        result = [col.find_one({'args': args})]
+
+        if result[0] is None:
+            url = f"http://dict.youdao.com/w/{args}/"
+            soup = BeautifulSoup(requests.get(url).text, features="html.parser")
+            trans = soup.find('div', {'id': 'phrsListTab'}).find_all("ul", limit=1)[0]
+            if trans:
+                trans = trans.text.strip('\n').split('\n')
+
+            pronounce = soup.find('div', {'id': 'phrsListTab'}).find("div", {"class": "baav"})
+            if pronounce:
+                pronounce = pronounce.text.strip('\n')
+
+            additional = soup.find('div', {'id': 'phrsListTab'}).find("p", {"class": "additional"})
+            if additional:
+                additional = additional.text
+            query = {"args": args, "trans": trans, "pronounce": pronounce, "additional": additional}
+            insert_mongo(query)
+
+            result = [col.find_one({'args': args})]
+        for res in result:
+            res["_id"] = str(res["_id"])
+
+        return jsonify({"code": 0, "data": result, "page_total": 1})
+    else:
+        page = int(request.args.get('page') or 1)
+        start = (page - 1) * PER_PAGE
+        result = list(col.find().sort('_id', 1).skip(start).limit(PER_PAGE))
+        for res in result:
+            res["_id"] = str(res["_id"])
+
+        return jsonify({"code": 0, "data": result, "page_total": math.ceil(col.count()/13)})
 
 
 if __name__ == '__main__':
